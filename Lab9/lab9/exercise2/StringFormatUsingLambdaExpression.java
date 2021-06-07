4d9ceeace67e35bb2c03209cfde11cb4e4f67ae3
Sneha2211 <84120041+Sneha-JA17@users.noreply.github.com>
2021-06-07 11:39:21 +0530

    Delete Lab9/lab9 directory

--- Lab9/lab9/exercise2/StringFormatUsingLambdaExpression.java
+++
@@ -1,8 +0,0 @@
-package lab9.exercise2;
-
-@FunctionalInterface
-public interface StringFormatUsingLambdaExpression {
-	
-	// method to insert a space between each characters of a given string
-	String returnExpression(String string);
-}
--- Lab9/lab9/exercise5/FactorialUsingLambdaExpression.java
+++
@@ -1,8 +0,0 @@
-package lab9.exercise5;
-
-@FunctionalInterface
-public interface FactorialUsingLambdaExpression {
-
-	// function to calculate factorial of a number
-	int factorial(int number);
-}
