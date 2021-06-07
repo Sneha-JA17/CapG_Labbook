690b87c90a867cbab78c20d4fa968a8ff1f4a0d5
Sneha2211 <84120041+Sneha-JA17@users.noreply.github.com>
2021-06-07 11:44:35 +0530

    Add files via upload

---
+++ lab9/exercise1/ExponentsUsingLambdaExpression.java
@@ -0,0 +1,11 @@
+package lab9.exercise1;
+
+
+	@FunctionalInterface
+	public interface ExponentsUsingLambdaExpression {
+
+		// method to calculate the value of x raised to the power of y
+		int power(int x,int y);
+		
+	}
+
---
+++ lab9/exercise2/StringFormatUsingLambdaExpression.java
@@ -0,0 +1,8 @@
+package lab9.exercise2;
+
+@FunctionalInterface
+public interface StringFormatUsingLambdaExpression {
+	
+	// method to insert a space between each characters of a given string
+	String returnExpression(String string);
+}
