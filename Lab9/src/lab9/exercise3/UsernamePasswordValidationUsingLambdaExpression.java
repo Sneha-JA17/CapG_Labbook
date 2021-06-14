8d4c4fa59827c11ffc719e93f54a0b0124f52d42
Sneha2211 <84120041+Sneha-JA17@users.noreply.github.com>
2021-06-15 02:20:45 +0530

    Add files via upload

---
+++ Lab9/src/lab9/exercise3/UsernamePasswordValidationUsingLambdaExpression.java
@@ -0,0 +1,8 @@
+package lab9.exercise3;
+
+@FunctionalInterface
+public interface UsernamePasswordValidationUsingLambdaExpression {
+
+	//boolean method to validate user name & password
+	boolean validateusername_password(String username,String password);
+}
