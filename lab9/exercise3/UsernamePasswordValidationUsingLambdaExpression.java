73231e00fc69a7a163e7e17b78a180aa6272a5e6
Sneha2211 <84120041+Sneha-JA17@users.noreply.github.com>
2021-06-15 02:19:11 +0530

    Delete lab9 directory

--- lab9/exercise3/UsernamePasswordValidationUsingLambdaExpression.java
+++
@@ -1,8 +0,0 @@
-package lab9.exercise3;
-
-@FunctionalInterface
-public interface UsernamePasswordValidationUsingLambdaExpression {
-
-	//boolean method to validate user name & password
-	boolean validateusername_password(String username,String password);
-}
